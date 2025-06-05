b960da7573d9ffe0c706b15b8c0fc75e960df041
gabriel <gabrielfgsantos95@gmail.com>
2025-06-04 21:37:06 -0300

    fix: engtrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ alembic upgrade head
 
 echo "Postgres started"
 # Start the FastAPI application with Uvicorn
-exec uvicorn app.main:app --host 0.0.0.0 --port 8080 --proxy-headers --forwarded-allow-ips "*"
+exec uvicorn src.main:app --host 0.0.0.0 --port 8080 --proxy-headers --forwarded-allow-ips "*"
